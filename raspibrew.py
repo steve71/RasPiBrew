9bf19d9c3c0982caacfba7780628a2102b514e7a
Stephen Smith <steve028@yahoo.com>
2012-07-01 20:26:31 -0400

    added pid control

--- raspibrew.py
+++ raspibrew.py
@@ -26,11 +26,12 @@ from datetime import datetime
 import web, time, random, json
 #uncomment for raspberry pi
 from smbus import SMBus
+from pid import pid as PIDController
 
 
 urls = ("/", "raspibrew",
         "/getrand", "getrand",
-        "/gettemp", "gettemp")
+        "/getstatus", "getstatus")
 
 render = web.template.render("templates/")
 
@@ -48,15 +49,15 @@ class raspibrew:
         self.cycle_time = 2.0
         self.duty_cycle = 0.0
         self.set_point = 0.0
-        self.p_param = 0.0
-        self.i_param = 0.0
-        self.d_param = 0.0
+        self.k_param = 2.39
+        self.i_param = 489
+        self.d_param = 44.9
         
         if runonce:
             parent_conn, child_conn = Pipe()       
             #rename to tempControlProc to test with raspberry pi    
             p = Process(name = "tempControlProc", target=tempControlProc, args=(self.mode, self.cycle_time, self.duty_cycle, \
-                                                          self.set_point, self.p_param, self.i_param, self.d_param, \
+                                                          self.set_point, self.k_param, self.i_param, self.d_param, \
                                                           child_conn))
             p.start()
             runonce = False
@@ -64,7 +65,7 @@ class raspibrew:
     def GET(self):
        
         return render.raspibrew(self.mode, self.set_point, self.duty_cycle, self.cycle_time, \
-                                self.p_param,self.i_param,self.d_param)
+                                self.k_param,self.i_param,self.d_param)
         
     def POST(self):
         global parent_conn
@@ -82,14 +83,14 @@ class raspibrew:
             if datalistkey[0] == "cycletime":
                 self.cycle_time = float(datalistkey[1])
             if datalistkey[0] == "p":
-                self.p_param = float(datalistkey[1])
+                self.k_param = float(datalistkey[1])
             if datalistkey[0] == "i":
                 self.i_param = float(datalistkey[1])
             if datalistkey[0] == "d":
                 self.d_param = float(datalistkey[1])
             
         parent_conn.send([self.mode, self.cycle_time, self.duty_cycle, self.set_point, \
-                              self.p_param, self.i_param, self.d_param])  
+                              self.k_param, self.i_param, self.d_param])  
              
             #mode, cycle_time, duty_cycle = parent_conn.recv()
             
@@ -98,7 +99,7 @@ class raspibrew:
             #print duty_cycle
                
         #return render.raspibrew(self.mode,self.set_point, self.duty_cycle, self.cycle_time, \
-        #                        self.p_param,self.i_param,self.d_param)
+        #                        self.k_param,self.i_param,self.d_param)
  
 def getrandProc(conn):
     p = current_process()
@@ -119,7 +120,7 @@ def gettempProc(conn):
         #t = time.time()
         num = tempdata()
         #elapsed = time.time() - t
-        time.sleep(.5) #.5+~.83 = ~1.33 seconds
+        time.sleep(.05) #.5+~.83 = ~1.33 seconds
         conn.send(num)
 
 def getonofftime(cycle_time, duty_cycle):
@@ -151,24 +152,27 @@ def heatProc(cycle_time, duty_cycle, conn):
     bus = SMBus(0)
     bus.write_byte_data(0x20,0x00,0x00) #set I/0 to write
     while (True):
-        if (conn.poll()):
+        while (conn.poll()): #get last
             cycle_time, duty_cycle = conn.recv()
-            on_time, off_time = getonofftime(cycle_time, duty_cycle)
+        conn.send([cycle_time, duty_cycle])    
         if duty_cycle == 0:
             bus.write_byte_data(0x20,0x09,0x00)
+            time.sleep(cycle_time)
         elif duty_cycle == 100:
             bus.write_byte_data(0x20,0x09,0x01)
+            time.sleep(cycle_time)
         else:
             on_time, off_time = getonofftime(cycle_time, duty_cycle)
             bus.write_byte_data(0x20,0x09,0x01)
             time.sleep(on_time)
             bus.write_byte_data(0x20,0x09,0x00)
             time.sleep(off_time)
+        
         #y = datetime.now()
         #time_sec = y.second + y.microsecond/1000000.0
         #print "%s Thread time (sec) after LED off: %.2f" % (self.getName(), time_sec)
 
-def tempControlProcTest(mode, cycle_time, duty_cycle, set_point, p_param, i_param, d_param, conn):
+def tempControlProcTest(mode, cycle_time, duty_cycle, set_point, k_param, i_param, d_param, conn):
     
         p = current_process()
         print 'Starting:', p.name, p.pid
@@ -184,20 +188,20 @@ def tempControlProcTest(mode, cycle_time, duty_cycle, set_point, p_param, i_para
         while (True):
             if parent_conn_temp.poll():
                 randnum = parent_conn_temp.recv() #non blocking receive
-                conn.send([randnum, mode, cycle_time, duty_cycle, set_point, p_param, i_param, d_param])
+                conn.send([randnum, mode, cycle_time, duty_cycle, set_point, k_param, i_param, d_param])
             if parent_conn_heat.poll():
                 cycle_time, duty_cycle = parent_conn_heat.recv()
                 #duty_cycle = on_time/offtime*100.0
                 #cycle_time = on_time + off_time
             if conn.poll():
-                mode, cycle_time, duty_cycle, set_point, p_param, i_param, d_param = conn.recv()
+                mode, cycle_time, duty_cycle, set_point, k_param, i_param, d_param = conn.recv()
                 #conn.send([mode, cycle_time, duty_cycle])
                 #if mode == "manual": 
                 parent_conn_heat.send([cycle_time, duty_cycle])
             
 #controls 
 
-def tempControlProc(mode, cycle_time, duty_cycle, set_point, p_param, i_param, d_param, conn):
+def tempControlProc(mode, cycle_time, duty_cycle, set_point, k_param, i_param, d_param, conn):
     
         p = current_process()
         print 'Starting:', p.name, p.pid
@@ -211,23 +215,43 @@ def tempControlProc(mode, cycle_time, duty_cycle, set_point, p_param, i_param, d
         pheat.start()  
         
         while (True):
-            if parent_conn_temp.poll():
-                temp = parent_conn_temp.recv() #blocking receive
-                conn.send([temp, mode, cycle_time, duty_cycle, set_point, p_param, i_param, d_param])
-            if parent_conn_heat.poll(): #non blocking receive
+            readytemp = False
+            while parent_conn_temp.poll():
+                temp = parent_conn_temp.recv() #non blocking receive    
+                readytemp = True
+            if readytemp == True:
+                conn.send([temp, mode, cycle_time, duty_cycle, set_point, k_param, i_param, d_param]) #GET request
+                readytemp == False
+            readyheat = False    
+            while parent_conn_heat.poll(): #non blocking receive
                 cycle_time, duty_cycle = parent_conn_heat.recv()
+                readyheat = True
+            if readyheat == True:
                 if mode == "auto":
-                    #todo calculate pid and update heatProc every cycle
-                    #calculate PID
-                    #parent_conn_heat.send([cycle_time, duty_cycle])
-                    pass
-            if conn.poll():
-                mode, cycle_time, duty_cycle, set_point, p_param, i_param, d_param = conn.recv()
+                    #calculate PID every cycle
+                    duty_cycle = pid.calcPID(float(temp), float(set_point), 1)
+                    #send to heat process every cycle
+                    parent_conn_heat.send([cycle_time, duty_cycle])     
+                    readyheat = False     
+            readyPOST = False
+            while conn.poll(): #POST settings
+                mode, cycle_time, duty_cycle_temp, set_point, k_param, i_param, d_param = conn.recv()
+                readyPOST = True
+            if readyPOST == True:
+                if mode == "auto":
+                    print "auto selected"
+                    pid = PIDController.PID(cycle_time, k_param, i_param, d_param) #init pid
+                    duty_cycle = pid.calcPID(float(temp), float(set_point), 1)
+                    parent_conn_heat.send([cycle_time, duty_cycle])  
                 if mode == "manual": 
-                    parent_conn_heat.send([cycle_time, duty_cycle])
+                    print "manual selected"
+                    duty_cycle = duty_cycle_temp
+                    parent_conn_heat.send([cycle_time, duty_cycle])    
                 if mode == "off":
+                    print "off selected"
                     duty_cycle = 0
                     parent_conn_heat.send([cycle_time, duty_cycle])
+                readyPOST = False
             time.sleep(.01)
                     
 class getrand:
@@ -235,14 +259,14 @@ class getrand:
         pass
     def GET(self):
         global parent_conn  
-        randnum, mode, cycle_time, duty_cycle, set_point, p_param, i_param, d_param = parent_conn.recv()
+        randnum, mode, cycle_time, duty_cycle, set_point, k_param, i_param, d_param = parent_conn.recv()
         #controlData = parent_conn.recv()
         out = json.dumps({"temp" : randnum,
                           "mode" : mode,
                     "cycle_time" : cycle_time,
                     "duty_cycle" : duty_cycle,
                      "set_point" : set_point,
-                       "p_param" : p_param,
+                       "k_param" : k_param,
                        "i_param" : i_param,
                        "d_param" : d_param})  
         return out
@@ -251,19 +275,19 @@ class getrand:
     def POST(self):
         pass
 
-class gettemp:
+class getstatus:
     def __init__(self):
         pass    
 
     def GET(self):
         global parent_conn
-        temp, mode, cycle_time, duty_cycle, set_point, p_param, i_param, d_param = parent_conn.recv()
-        out = json.dumps({"temp" : randnum,
+        temp, mode, cycle_time, duty_cycle, set_point, k_param, i_param, d_param = parent_conn.recv()
+        out = json.dumps({"temp" : temp,
                           "mode" : mode,
                     "cycle_time" : cycle_time,
                     "duty_cycle" : duty_cycle,
                      "set_point" : set_point,
-                       "p_param" : p_param,
+                       "k_param" : k_param,
                        "i_param" : i_param,
                        "d_param" : d_param})  
         return out
