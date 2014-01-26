45b6a6a51e766631a9b504dde2877a509bb97147
unknown <Steve@.(none)>
2014-01-26 09:35:58 -0500

    update

--- static/raspibrew.js
+++ static/raspibrew.js
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2014 Stephen P. Smith
+// Copyright (c) 2012-2014 Stephen P. Smith
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files
